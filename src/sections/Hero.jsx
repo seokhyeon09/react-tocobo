11a611d21aa7c8172c5c44f58b83f3c51997824c
seokhyeon09 <ring4392@gmail.com>
2026-03-04 20:14:19 +0900

    mob-section update

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Swiper, SwiperSlide } from 'swiper/react';
 import './styles/Hero.scss'
 
@@ -11,6 +11,15 @@ import { Pagination, Navigation } from 'swiper/modules';
 
 import { heroSlides } from '../util/hero';
 const Hero = () => {
+  const [isMobile, setIsMobile] = useState(
+    window.innerWidth<=1111
+  )
+  useEffect(()=>{
+    const handleResize = () => setIsMobile(window.innerWidth<=1111)
+    window.addEventListener('resize', handleResize)
+    return ()=>window.removeEventListener('resize', handleResize)
+  }, [])
+
   return (
     <div className="hero-container">
       <Swiper
@@ -29,7 +38,7 @@ const Hero = () => {
         {heroSlides.map((slide) => (
           <SwiperSlide
           className={`hero-slide ${slide.id}`}
-          style={{backgroundImage:`url(${slide.image.desktop})`}}
+          style={{backgroundImage:`url(${isMobile? slide.image.mobile : slide.image.desktop})`}}
           key={slide.id}
           >
             <div className="inner">
